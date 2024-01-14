2c449272cfd9f1b347b738a0a59679f9ddb73cb9
theseedubya <101205727+theseedubya@users.noreply.github.com>
2024-01-14 02:09:53 +0000

    Added asda pizza calorie data. Updated readme.

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Sources for the data used within the calculator should be provided below.
 
 ### Subway
 https://www.subway.com/-/media/United_Kingdom/2023/W3/IngredientsNutritionalInformationMay2023.pdf
+
+### Asda
+This is the calories for the create your own pizza. Data is taken from a photo of a sign of the nutriton information taken at the counter by theseedubya. 
---
+++ asdaPizza/data/calories.js
@@ -0,0 +1,50 @@
+const bases = 
+{
+  "thin stonebaked base": [{"small":282},{"medium":494},{"large":960}],
+  "woodfired":[{"medium":496},{"large":808}],
+  "stuffed crust":[{"medium":984}],
+  "deep pan":[{"medium":778},{"large":1916}],
+  "detroit": [{"large":900}]
+};
+
+const sauces = 
+{
+  "small":[],
+  "medium":[{"tomato sauce":27,"bbq sauce":93, "garlic spread":223}],
+  "large":[{"tomato sauce":53,"bbq sauce":187, "garlic spread":447}],
+};
+
+const cheeses = {
+  "small":[],
+  "medium":[{"grated cheese":147,"vegan cheese":250}],
+  "large":[{"grated cheese":327,"vegan cheese":500}]
+};
+
+const toppings = 
+{
+  "small":[],
+  "medium":[{"smoked ham":40,"spicy chicken":59,"plain chicken":43,"diced chorizo":129,"bacon":113,
+             "spicy beef":67,"diced steak":37,"pepperoni":63,"meatballs":93,"mushrooms":4,
+             "red onions":11,"mixed peppers":10,"sweetcorn":28,"pineapple pieces":27,"jalepeno peppers":1,
+             "bbq jackfruit":43,"sliced black olives":61,"chilli cheese":140,"mozzarella slices":65}
+           ],
+  "large":[{"smoked ham":81,"spicy chicken":118,"plain chicken":85,"diced chorizo":259,"bacon":226,
+             "spicy beef":134,"diced steak":73,"pepperoni":126,"meatballs":186,"mushrooms":8,
+             "red onions":22,"mixed peppers":21,"sweetcorn":55,"pineapple pieces":55,"jalepeno peppers":3,
+             "bbq jackfruit":86,"sliced black olives":122,"chilli cheese":279,"mozzarella slices":108}
+           ],
+}
+
+const toppedCheeses =  {
+  "small":[],
+  "medium":[{"grated cheese":80}],
+  "large":[{"grated cheese":159}]
+}
+
+module.exports = {
+    bases,
+    sauces,
+    cheeses,
+    toppings,
+    toppedCheeses
+}
